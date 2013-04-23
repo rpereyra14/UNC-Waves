38e2385e7aa3e1758dc9ea64b213400b268f6dfc
Steven Douglas Love <slove13@live.unc.edu>
2013-04-23 18:12:07 -0400

    interactive movies almost done

--- UI/average.m
+++ UI/average.m
@@ -18,29 +18,29 @@ function [ interpWave ] = average( fineExcitation, dimensions )
         
     output_size = dimensions;
     
-    scale = [input_size(1)*output_size(1) input_size(2)*output_size(2) input_size(3)*output_size(3)];
+    scale = [input_size(1)*output_size(1) input_size(2)*output_size(2) input_size(3)];% excluding extra time factor *output_size(3)];
     interpWaveScaled = zeros(scale);
   
     
     %CHOOSE THIS 
-   for i = 1:input_size(3)
-       for j = 1:output_size(3)
-           interpWaveScaled(:,:,(i-1)*output_size(3)+j) = kron(fineExcitation(:,:,i),ones(output_size(1), output_size(2)));     
-       end
-   end
+%   for i = 1:input_size(3)
+%       for j = 1:output_size(3)
+%           interpWaveScaled(:,:,(i-1)*output_size(3)+j) = kron(fineExcitation(:,:,i),ones(output_size(1), output_size(2)));     
+%       end
+%   end
  %OR THIS
  
-%     for i = 1:input_size(1)
-%         for j = 1:input_size(2)
-%             for k = 1:input_size(3)
-%                 x = ((i-1)*output_size(1))+1:(i*output_size(1));
-%                 y = ((j-1)*output_size(2))+1:(j*output_size(2));
-%                 z = ((k-1)*output_size(3))+1:(k*output_size(3));
-%                 
-%                 interpWaveScaled(x, y, z) = fineExcitation(i, j, k);
-%             end
-%         end
-%     end
+    for i = 1:input_size(1)
+        for j = 1:input_size(2)
+            for k = 1:input_size(3)
+                x = ((i-1)*output_size(1))+1:(i*output_size(1));
+                y = ((j-1)*output_size(2))+1:(j*output_size(2));
+                z = k;%((k-1)*output_size(3))+1:(k*output_size(3));
+                
+                interpWaveScaled(x, y, z) = fineExcitation(i, j, k);
+            end
+        end
+    end
 
 %END CHOICE
             
@@ -53,7 +53,7 @@ function [ interpWave ] = average( fineExcitation, dimensions )
                 % will be averaged
                 x = ((i-1)*input_size(1))+1:(i*input_size(1));
                 y = ((j-1)*input_size(2))+1:(j*input_size(2));
-                z = ((k-1)*input_size(3))+1:(k*input_size(3));
+                z = k;%((k-1)*input_size(3))+1:(k*input_size(3));
                 
                 interpWave(i, j, k) = mean(mean(mean(interpWaveScaled(x, y, z))));
             end
---
+++ UI/surfRender.m
@@ -0,0 +1,51 @@
+function[F] = surfRender(orig,interpolated,frame,axis1,axis2)
+    
+
+    input_size = size(orig);
+    yrange = [1 input_size(1)];
+    zrange = [1 input_size(2)];
+    eXcitationrange = [min(min(min(orig))) max(max(max(orig)))];    
+
+%INTERPOLATION
+	interp_size = size(interpolated);
+   
+    i_yrange = [1 interp_size(1)];
+    i_zrange = [1 interp_size(2)];
+
+    %we'll use the same z range
+
+ 
+%PREPARING THE FIGURE    
+    %axis tight
+    %set(axis_handle,'nextplot','replacechildren');
+    
+    %fig = figure('Renderer','zbuffer','visible','off','Color',[240 240 240]/255);
+   
+%     if strcmp(method,'embed')
+%         set(fig, 'Position', [475, 0, 700, 675]);
+%     else
+%         set(fig, 'Position', [100 100 800 600]);
+%     end
+     
+  
+%PLOTTING
+   %for each time slice
+%    axes(axis_handle);
+        %axes(axis1);
+        %disp(size(orig(:,:,frame)));
+        %campos(axis1,view1);
+        surf(axis1,orig(:,:,frame));%draw the 3d figure
+        %camorbit(orbity,orbitz,'camera');
+       % disp('b');
+        axis(axis1,[1 zrange(2) 1 yrange(2) eXcitationrange(1) eXcitationrange(2)]);
+
+        
+        
+        %subplot(1,2,2);
+           %    campos(axis2,view2);
+
+        surf(axis2,interpolated(:,:,frame));%draw the interpolated version
+            %   camorbit(orbity,orbitz,'camera');
+        axis(axis2,[i_zrange(1) i_zrange(2) i_yrange(1) i_yrange(2) eXcitationrange(1)-1 eXcitationrange(2)+1]);
+
+
