ed478c170800b1d6c52d3df4745e3442f85b3167
Christopher Mullins <christopher.mullins@kitware.com>
2013-05-01 16:47:55 -0300

    Gets rid of need to extract tarball.
    User should only need the source to build the wavetank simulator.

---
+++ LEWOS/UNC-Waves_Simulator/Record.h
@@ -0,0 +1,26 @@
+class Record{
+
+public:
+
+	Record( int domain, int channel, int t, int val );
+	int domainID;
+	int channelID;
+	int timeOffset;
+	int actuatorValue;
+
+};
+
+Record::Record( int domain, int channel, int t, int val ){
+
+	Record::domainID = domain;
+	Record::channelID = channel;
+	Record::timeOffset = t;
+	Record::actuatorValue = val;
+
+};
+
+struct comparator{
+	bool operator()(Record* a, Record* b ){
+		return (*a).domainID < (*b).domainID || (*a).channelID < (*b).channelID || (*a).timeOffset < (*b).timeOffset;
+	}
+} compareRecords;
