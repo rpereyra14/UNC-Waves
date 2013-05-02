351fd53bb013ee6793a887eb144fa0b3e40ad959
Renato Pereyra <rpereyra14@gmail.com>
2013-05-01 23:34:59 -0400

    adding testing framework for LEWOS input parser

---
+++ LEWOS/UNC-Waves_Simulator/tester/CTestTestfile.cmake
@@ -0,0 +1,23 @@
+# CMake generated Testfile for 
+# Source directory: /home/rpereyra/LEWOS/UNC-Waves_Simulator/tester
+# Build directory: /home/rpereyra/LEWOS/UNC-Waves_Simulator/tester
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(bad_time1 "./UNC-Waves_tester" "bad_time1.txt")
+ADD_TEST(bad_value1 "./UNC-Waves_tester" "bad_value1.txt")
+ADD_TEST(bad_value2 "./UNC-Waves_tester" "bad_value2.txt")
+ADD_TEST(bad_y_offset1 "./UNC-Waves_tester" "bad_y_offset1.txt")
+ADD_TEST(bad_y_offset2 "./UNC-Waves_tester" "bad_y_offset2.txt")
+ADD_TEST(bad_z_offset1 "./UNC-Waves_tester" "bad_z_offset1.txt")
+ADD_TEST(bad_z_offset2 "./UNC-Waves_tester" "bad_z_offset2.txt")
+ADD_TEST(improper_format1 "./UNC-Waves_tester" "improper_format1.txt")
+ADD_TEST(improper_format2 "./UNC-Waves_tester" "improper_format2.txt")
+ADD_TEST(improper_format3 "./UNC-Waves_tester" "improper_format3.txt")
+ADD_TEST(improper_format4 "./UNC-Waves_tester" "improper_format4.txt")
+ADD_TEST(improper_format5 "./UNC-Waves_tester" "improper_format5.txt")
+ADD_TEST(pass1 "./UNC-Waves_tester" "pass1.txt")
+ADD_TEST(pass2 "./UNC-Waves_tester" "pass2.txt")
+ADD_TEST(pass3 "./UNC-Waves_tester" "pass3.txt")
+ADD_TEST(pass4 "./UNC-Waves_tester" "pass4.txt")
+ADD_TEST(pass5 "./UNC-Waves_tester" "pass5.txt")
---
+++ LEWOS/UNC-Waves_Simulator/tester/UNC-Waves_tester.cpp
@@ -0,0 +1,58 @@
+/*******************************************************
+
+	TESTING FRAMEWORK FOR LEWOS INPUT FILE FORMATS
+
+
+*******************************************************/
+
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <iostream>
+
+int main( int argc, char* argv[] ){
+
+	if( argc != 2 ){
+		std::cout << "Usage: ./UNC-Waves_tester <input_test_file>" << std::endl ;
+		exit( 1 );
+	}
+
+	std::string temp( argv[1] );
+
+	char cmd[200] = "";
+	cmd[0] = '\0';
+	strcat( cmd, "../MACRO_UNC-Waves_Simulator " );
+	strcat( cmd, temp.c_str() );
+	int ret = system( cmd );
+
+	if( temp.find( "pass", 0 ) != std::string::npos && ret == 0 ){
+		std::cout << 0 << std::endl;
+		return 0;
+	}else if( temp.find( "pass", 0 ) == std::string::npos && ret != 0 ){ 
+		if( temp.find( "bad_file", 0 ) != std::string::npos && ret == 4 ){
+
+		}else if( temp.find( "improper_format", 0 ) != std::string::npos && ret == 5 ){
+			std::cout << 1 << std::endl;
+			return 1;
+		}else if( temp.find( "bad_value", 0 ) != std::string::npos && ret == 8 ){
+			std::cout << 1 << std::endl;
+			return 1;
+		}else if( temp.find( "bad_z_offset", 0 ) != std::string::npos && ret == 9 ){
+			std::cout << 1 << std::endl;
+			return 1;
+		}else if( temp.find( "bad_y_offset", 0 ) != std::string::npos && ret == 10 ){
+			std::cout << 1 << std::endl;
+			return 1;
+		}else if( temp.find( "bad_time", 0 ) != std::string::npos && ret == 11 ){
+			std::cout << 1 << std::endl;
+			return 1;
+		}else{
+			std::cout << 0 << std::endl;
+			return 0;
+		}
+	}else{
+		std::cout << 1 << std::endl;
+		return 1;
+	}
+
+}
